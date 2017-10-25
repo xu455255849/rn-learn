915b52ea2712257f6051266d5f222054bb8d3c5e
徐绍平 <455255849@qq.com>
2017-10-25 18:45:14 +0800

    初始化环境

---
+++ index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { DatePicker, List } from 'antd-mobile';
+import { AppRegistry, View } from 'react-native';
+
+export default class AntdMobieDemo extends Component {
+    render() {
+        return (<View style={{ marginTop: 30 }}>
+            <List>
+                <DatePicker mode="datetime">
+                    <List.Item arrow="horizontal">
+                        选择时间
+                    </List.Item>
+                </DatePicker>
+            </List>
+        </View>);
+    }
+}
+
+AppRegistry.registerComponent('androidDemo', () => AntdMobieDemo);
