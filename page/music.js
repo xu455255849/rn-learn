e4d030aa955cfe455979b5516a8670d36aa46d95
徐绍平 <455255849@qq.com>
2017-10-27 21:56:38 +0800

    音乐功能更换

--- page/music.js
+++ page/music.js
@@ -7,55 +7,7 @@ import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
 var Sound = require('react-native-sound');
 Sound.setCategory('Playback');
 
-var nide = new Sound('nide.mp3', Sound.MAIN_BUNDLE, (error) => {
-    if (error) {
-        console.log('failed to load the sound', error);
-        return;
-    }
-    // loaded successfully
-    console.log('duration in seconds: ' + nide.getDuration() + 'number of channels: ' + nide.getNumberOfChannels());
-});
-var qinai = new Sound('qinai.mp3', Sound.MAIN_BUNDLE, (error) => {
-    if (error) {
-        console.log('failed to load the sound', error);
-        return;
-    }
-    // loaded successfully
-    console.log('duration in seconds: ' + qinai.getDuration() + 'number of channels: ' + qinai.getNumberOfChannels());
-});
-var shi = new Sound('nide.mp3', Sound.MAIN_BUNDLE, (error) => {
-    if (error) {
-        console.log('failed to load the sound', error);
-        return;
-    }
-    // loaded successfully
-    console.log('duration in seconds: ' + shi.getDuration() + 'number of channels: ' + shi.getNumberOfChannels());
-});
-var tian = new Sound('tian.mp3', Sound.MAIN_BUNDLE, (error) => {
-    if (error) {
-        console.log('failed to load the sound', error);
-        return;
-    }
-    // loaded successfully
-    console.log('duration in seconds: ' + tian.getDuration() + 'number of channels: ' + tian.getNumberOfChannels());
-});
-var xiao = new Sound('nide.mp3', Sound.MAIN_BUNDLE, (error) => {
-    if (error) {
-        console.log('failed to load the sound', error);
-        return;
-    }
-    // loaded successfully
-    console.log('duration in seconds: ' + xiao.getDuration() + 'number of channels: ' + xiao.getNumberOfChannels());
-});
-var yong = new Sound('nide.mp3', Sound.MAIN_BUNDLE, (error) => {
-    if (error) {
-        console.log('failed to load the sound', error);
-        return;
-    }
-    // loaded successfully
-    console.log('duration in seconds: ' + yong.getDuration() + 'number of channels: ' + yong.getNumberOfChannels());
-});
-var yu = new Sound('nide.mp3', Sound.MAIN_BUNDLE, (error) => {
+var yu = new Sound('yu.mp3', Sound.MAIN_BUNDLE, (error) => {
     if (error) {
         console.log('failed to load the sound', error);
         return;
@@ -66,58 +18,11 @@ var yu = new Sound('nide.mp3', Sound.MAIN_BUNDLE, (error) => {
 
 
 export default class MusicScreen extends Component {
-    static navigationOptions = ({ navigation }) => ({
-        headerRight: <Button title="操作" onPress={ ()=> { Toast.info('操作操作操作操作操作', 1); }} />,
-        title: `Some Info`,
-    });
     constructor(props) {
         super(props);
         this.state = {
-            list: [
-                {
-                    play: false,
-                    title: 'HuaoH,HuaoH - 你的名字-黄昏之时（Escaperz Bootleg ）',
-                    sound: nide,
-                    key: '1'
-                },
-                {
-                    play: false,
-                    title: 'Minto薄荷糖,顾时笙 - 亲爱的你（Cover：葛雨晴 ／ 可歆）',
-                    sound: qinai,
-                    key: '2'
-                },
-                {
-                    play: false,
-                    title: '双笙,囧菌 - 世末歌者（Cover 乐正绫）',
-                    sound: shi,
-                    key: '3'
-                },
-                {
-                    play: false,
-                    title: '夏初安,君洛 - 甜点和诗（原曲：何洁《小永远》）',
-                    sound: tian,
-                    key: '4'
-                },
-                {
-                    play: false,
-                    title: '毛儿,朵芊 - 小城谣（Cover 胡碧乔）',
-                    sound: xiao,
-                    key: '5'
-                },
-                {
-                    play: false,
-                    title: 'Mi2 - 勇敢爱',
-                    sound: nide,
-                    key: '6'
-                },
-                {
-                    play: false,
-                    title: '兔裹煎蛋卷 - 鱼玄机',
-                    sound: yu,
-                    key: '7'
-                },
-            ]
-           
+            title: '兔裹煎蛋卷 - 鱼玄机',
+            play: false
         };
     }
     
@@ -125,41 +30,31 @@ export default class MusicScreen extends Component {
     
     }
     
-    play = (item, index)=> {
-        alert(item, index)
-        this.state.list[index].play = true
-        // item.sound.play();
-        this.setState({})
+    play = ()=> {
+        if (this.state.play) {
+            yu.pause();
+            this.setState({
+                play: !this.state.play
+            })
+        } else {
+            yu.play();
+            this.setState({
+                play: !this.state.play
+            })
+        }
     };
-    stop = (item, index)=> {
-        alert(JSON.stringify(item))
-        this.state.list[index].play = true
-        this.setState({})
-    }
     
     render() {
         return (
             <View style={styles.container}>
-                <FlatList
-                    data={this.state.list}
-                    keyExtractor={this._keyExtractor}
-                    renderItem={({item, index}) => (
-                        <View style={{ flexDirection: 'row', padding: 20, marginBottom: 10 }}>
-                            <View style={{ flex: 4, height: 80,  alignItems: 'center', justifyContent: 'center'}} >
-                                <Text>{item.title}</Text>
-                            </View>
-                            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', height: 80,}} >
-                                {
-                                    item.play ?
-                                        <Button color="#EE8094" title="暂停" onPress={this.stop(item, index)} />
-                                        :
-                                        <Button color="#95EE79" title="播放" onPress={this.play(item, index)} />
-                                }
-                            </View>
-                        </View>
-                    )}
-                />
+                <Text>{this.state.title}</Text>
                 
+                {
+                    this.state.play ?
+                        <Button color="#EE8094" title="暂停" onPress={this.play} />
+                        :
+                        <Button color="#95EE79" title="播放" onPress={this.play} />
+                }
              
             </View>
         );
